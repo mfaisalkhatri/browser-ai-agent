a4518f1ff7869310385c87081ceae054bfe464ac
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-09-08 18:53:53 +0530

    added local configuration changes in config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,12 +10,29 @@ function getEnv(name: string): string {
   return value;
 }
 
+function getOptionalEnv(name: string): string | undefined {
+  return process.env[name];
+}
+
+function getBrowserMode(): "local" | "cloud" {
+  const browser = process.env.BROWSER ?? "local";
+
+  if (browser !== "local" && browser != "cloud") {
+    throw new Error(
+      "Invalid BROWSER value `${browser}`. Expected  'local' or 'cloud'.",
+    );
+  }
+  return browser;
+}
+
 export const config = {
   ollamaModel: process.env.OLLAMA_MODEL ?? "qwen3:8b",
   temperature: Number(process.env.LLM_TEMPERATURE ?? "0.2"),
-  LT_USERNAME: getEnv("LT_USERNAME"),
-  LT_ACCESS_KEY: getEnv("LT_ACCESS_KEY"),
+  browser: getBrowserMode(),
+  headless: process.env.HEADLESS === "true",
+  LT_USERNAME: getOptionalEnv("LT_USERNAME"),
+  LT_ACCESS_KEY: getOptionalEnv("LT_ACCESS_KEY"),
   platformName: process.env.BROWSERCLOUD_PLATFORM_NAME ?? "Windows 11",
   browserName: process.env.BROWSERCLOUD_BROWSER_NAME ?? "Chrome",
-  browserVersion: Number(process.env.BROWSERCLOUD_BROWSER_VERSION ?? "latest"),
+  browserVersion: process.env.BROWSERCLOUD_BROWSER_VERSION ?? "latest",
 };
